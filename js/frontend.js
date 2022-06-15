e639300535acf55002ded28262a7d2859f3c300a
Thiago Benites <thi.fam2014@gmail.com>
2022-06-15 21:19:07 +0000

    Ajustes finais

--- js/frontend.js
+++ js/frontend.js
@@ -7,7 +7,7 @@
 // ********************************************************************
 // Valida preenchimento correto dos campos
 function ValidatesFields() {
-  var value = SelectSoilType.value;
+  let value = SelectSoilType.value;
   
   if (value == 'Selecione'){ // Valida se Tipo de Solo foi selecionado
     alert('Selecione um Tipo de Solo!');
@@ -19,13 +19,13 @@ function ValidatesFields() {
 
 // Bloqueia digitação de letras no evento keypress
 function OnlyNumber(evt) {
-  var theEvent = evt || window.event;
-  var key = theEvent.keyCode || theEvent.which;
+  let theEvent = evt || window.event;
+  let key = theEvent.keyCode || theEvent.which;
   
   key = String.fromCharCode( key );
   
   //var regex = /^[0-9.,]+$/; // Permite pontos e virgulas
-  var regex = /^[0-9]+$/;
+  let regex = /^[0-9]+$/;
   
   if( !regex.test(key) ) {
     theEvent.returnValue = false;
@@ -35,21 +35,61 @@ function OnlyNumber(evt) {
 
 // Calcula nível de umidade do solo
 function Calculate() {
-  var Humidity = InputHumidity.value;
+  let Humidity = InputHumidity.value;
+  let SoilType = SelectSoilType.value;
 
-  InputHumidity.value = '';
   AreaResponse.innerHTML = '';
 
   if (Humidity != ''){
     if (Humidity < 1 || Humidity > 100) {
-      alert('Nível de Umidade deve estar entre 1 e 100');
+      alert('Nível de Umidade deve estar entre 1 e 100!');
       InputHumidity.value = '';
       InputHumidity.focus();
     } else {
-      AreaResponse.innerHTML = 'Calculou';
+      AreaResponse.innerHTML = AnalyzeSoil(SoilType, Humidity);
     }
   } else { 
-    alert('Digite o Nível de Umidade');
+    alert('Digite o Nível de Umidade!');
     InputHumidity.focus();
+  } 
+}
+
+// Analiza solo
+function AnalyzeSoil(SoilType, HumidityLevel){
+
+  let Analyze;
+  
+  switch(SoilType) {
+    case 'Argila':
+      if (HumidityLevel > 0 && HumidityLevel <= 59) {
+        Analyze = 'Umidade do solo perigosamente baixa';
+      } else if(HumidityLevel > 59 && HumidityLevel <= 79){
+        Analyze = 'Irrigação a ser aplicada';
+      } else if(HumidityLevel > 79 && HumidityLevel <= 100){
+        Analyze = 'Irrigação não necessária';
+      }
+      break;
+    case 'Argilosa':
+      if (HumidityLevel > 0 && HumidityLevel <= 69) {
+        Analyze = 'Umidade do solo perigosamente baixa';
+      } else if(HumidityLevel > 69 && HumidityLevel <= 87){
+        Analyze = 'Irrigação a ser aplicada';
+      } else if(HumidityLevel > 87 && HumidityLevel <= 100){
+        Analyze = 'Irrigação não necessária';
+      }
+      break;
+    case 'Arenosa':
+      if (HumidityLevel > 0 && HumidityLevel <= 79) {
+        Analyze = 'Umidade do solo perigosamente baixa';
+      } else if(HumidityLevel > 79 && HumidityLevel <= 89){
+        Analyze = 'Irrigação a ser aplicada';
+      } else if(HumidityLevel > 89 && HumidityLevel <= 100){
+        Analyze = 'Irrigação não necessária';
+      }
+      break;
+    default:
+      Analyze = '';
   }
+
+  return Analyze;
 }
